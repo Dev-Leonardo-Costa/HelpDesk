04ca9ba76ccb6d2e7603a3447699d7ab8093922a
Leonardo Costa <devleonardo27@gmail.com>
2023-10-08 12:08:54 -0300

    docs: add OpenAPI docs

--- usar-service-api/src/main/java/br/com/leonardo/controller/UserController.java
+++ usar-service-api/src/main/java/br/com/leonardo/controller/UserController.java
@@ -1,18 +1,36 @@
 package br.com.leonardo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import models.exceptions.StandarError;
 import models.responses.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Tag(name = "UserController", description = "Controller responsible for user operations")
 @RequestMapping("/api/users")
 public interface UserController {
 
+
+    @Operation(summary = "Find user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = StandarError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = StandarError.class)))
+    })
     @GetMapping("/{id}")
-    ResponseEntity<UserResponse> findById(@PathVariable final String id);
+    ResponseEntity<UserResponse> findById(
+            @Parameter(description = "user id", required = true, example = "651eba7f38db893ce983ce9b")
+            @PathVariable final String id);
 
 
 }
