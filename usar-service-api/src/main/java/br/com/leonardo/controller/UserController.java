9c7fafd65d537c0a1a71eb7a134f1b5543da1723
Leonardo Costa <devleonardo27@gmail.com>
2023-10-08 18:07:42 -0300

    feat: add endpoint to save user

--- usar-service-api/src/main/java/br/com/leonardo/controller/UserController.java
+++ usar-service-api/src/main/java/br/com/leonardo/controller/UserController.java
@@ -8,11 +8,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import models.exceptions.StandarError;
+import models.requests.CreateUserRequest;
 import models.responses.UserResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "UserController", description = "Controller responsible for user operations")
 @RequestMapping("/api/users")
@@ -22,9 +22,9 @@ public interface UserController {
     @Operation(summary = "Find user by id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "User found"),
-            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = "application/json",
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                     schema = @Schema(implementation = StandarError.class))),
-            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json",
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                     schema = @Schema(implementation = StandarError.class)))
     })
     @GetMapping("/{id}")
@@ -33,4 +33,14 @@ public interface UserController {
             @PathVariable final String id);
 
 
+    @Operation(summary = "Save new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created"),
+            @ApiResponse(responseCode = "400", description = "User not found", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = StandarError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = StandarError.class)))
+    })
+    @PostMapping
+    ResponseEntity<Void> save(@RequestBody final CreateUserRequest createUserRequest);
 }
--- usar-service-api/src/main/java/br/com/leonardo/controller/impl/UserControllerImpl.java
+++ usar-service-api/src/main/java/br/com/leonardo/controller/impl/UserControllerImpl.java
@@ -3,9 +3,13 @@ package br.com.leonardo.controller.impl;
 import br.com.leonardo.controller.UserController;
 import br.com.leonardo.service.UserService;
 import lombok.RequiredArgsConstructor;
+import models.requests.CreateUserRequest;
 import models.responses.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +20,12 @@ public class UserControllerImpl implements UserController {
     @Override
     public ResponseEntity<UserResponse> findById(String id) {
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @Override
+    public ResponseEntity<Void> save(final CreateUserRequest createUserRequest) {
+        service.save(createUserRequest);
+        return ResponseEntity.status(CREATED.value()).build();
     }
 
 
