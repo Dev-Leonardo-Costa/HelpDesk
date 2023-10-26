8f4c2fe609ce9a57d85cb1735b5a40778ed406d2
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2023-10-26 16:32:59 -0300

    test: add test integration find by id user not found controller

--- usar-service-api/src/test/java/br/com/leonardo/controller/impl/UserControllerImplTest.java
+++ usar-service-api/src/test/java/br/com/leonardo/controller/impl/UserControllerImplTest.java
@@ -3,15 +3,18 @@ package br.com.leonardo.controller.impl;
 import br.com.leonardo.creator.CreatorUtils;
 import br.com.leonardo.entity.User;
 import br.com.leonardo.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 
 import static br.com.leonardo.creator.CreatorUtils.genarateMock;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +30,7 @@ class UserControllerImplTest {
     private UserRepository repository;
 
     @Test
+    @DisplayName("Find user by id sucess")
     void testFindByIdWithSuccess() throws Exception {
 
         final var entity = genarateMock(User.class);
@@ -41,6 +45,19 @@ class UserControllerImplTest {
                 .andExpect(jsonPath("$.profiles").isArray());
 
         repository.deleteById(userId);
+    }
+
+    @Test
+    @DisplayName("Find user by id not found")
+    void testFindByIdWithNotFound() throws Exception {
+
+        mockMvc.perform(get("/api/users/{id}", "123"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Object not found. Id: 123, Type: UserResponse"))
+                .andExpect(jsonPath("$.error").value(NOT_FOUND.getReasonPhrase()))
+                .andExpect(jsonPath("$.path").value("/api/users/123"))
+                .andExpect(jsonPath("$.status").value(NOT_FOUND.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty());
     }
 
 }
